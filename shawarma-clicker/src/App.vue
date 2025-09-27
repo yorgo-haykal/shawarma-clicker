3fc158c2ed5fdecd435cf633a71a5088ba77f5a1
Yorgo Haykal <yorgohaykal004@gmail.com>
2025-09-27 21:36:01 +0200

    Removed the about page

--- shawarma-clicker/src/App.vue
+++ shawarma-clicker/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <h1>SHAWARMA CLICKER</h1>
   <router-view />
 </template>
 
